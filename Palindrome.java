aa49e095e55dde02f9d8f5a034e97ace0ea9017e
orhadar1 <or.hadar01@post.runi.ac.il>
2025-12-26 20:14:54 +0200

    palindrom

--- Palindrome.java
+++ Palindrome.java
@@ -8,8 +8,8 @@ public class Palindrome {
 	/** Checks if the given string is a palindrome. */
 	public static boolean isPalindrome(String s) {
 		if (s.length() <= 1) return true;
-		else if(s.charAt(0) == s.charAt(s.length() - 1))
-			return isPalindrome(s.substring(1,s.length()));
+		if(s.charAt(0) == s.charAt(s.length() - 1))
+		return isPalindrome(s.substring(1,s.length() - 1));
 		else return false;
     }
 }
