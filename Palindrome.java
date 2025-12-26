6e725a6eb581f309cb07cf88c3fa262131111e5b
orhadar1 <or.hadar01@post.runi.ac.il>
2025-12-26 20:10:37 +0200

    palindrom

--- Palindrome.java
+++ Palindrome.java
@@ -9,7 +9,7 @@ public class Palindrome {
 	public static boolean isPalindrome(String s) {
 		if (s.length() <= 1) return true;
 		else if(s.charAt(0) == s.charAt(s.length() - 1))
-			return isPalindrome(s.substring(1,s.length() - 1 ));
+			return isPalindrome(s.substring(1,s.length()));
 		else return false;
     }
 }
